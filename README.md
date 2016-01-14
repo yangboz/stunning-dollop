b48feb3b8570e6cb28813baa63b6e95609a1202d
yangboz <zhou.yangbo@intellif.com>
2016-01-14 10:02:48 +0800

    ADDED:maven project feature files

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # stunning-dollop
 Walk through conference track management.
 
+# TODO:
+
+OptaPlanner version: http://docs.jboss.org/optaplanner/release/6.3.0.Final/optaplanner-docs/html_single/index.html#plannerIntroduction
+
 # References
 
 http://www.theprojectspot.com/tutorial-post/creating-a-genetic-algorithm-for-beginners/3
---
+++ src/main/java/info/smartkit/ctm/StunningDollopApplication.java
@@ -0,0 +1,12 @@
+package info.smartkit.ctm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StunningDollopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StunningDollopApplication.class, args);
+	}
+}
